b7cb219a3e59d73c58d104fd179c54197a6b17bc
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-01-16 02:23:55 +0500

    Project Initialization and Basic Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from '@/components/sections/Hero'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div>
+    <Hero />
+    
+   </div>
+  )
+}
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      
+      <div>
+        <h1>
+          <span>Muhammad</span>
+          <span>Hassaan</span>
+        </h1>
+        <span>Modern Full Stack Developer</span>
+      </div>
+    
+    </div>
+  )
+}
+
+export default Hero
